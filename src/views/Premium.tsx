12d40e74d057e74b4efe3b4f6e41adb8866c043b
popop098 <53284350+popop098@users.noreply.github.com>
2020-12-17 11:13:47 +0900

    Update Premium.tsx

--- src/views/Premium.tsx
+++ src/views/Premium.tsx
@@ -98,7 +98,7 @@ class Premium extends Component {
                     marginTop: 100,
                     width: '50%'
                 }}>
-                  <p>`프리미엄 서비스에 가입하시면 쿨타임 무시, 광고 제거, '\n'펫 길들이기 호감도 2~5랜덤 상승의 혜택이 주어집니다.`</p>
+                  <p>프리미엄 서비스에 가입하시면 쿨타임 무시, 광고 제거, 펫 길들이기 호감도 2~5랜덤 상승의 혜택이 주어집니다.</p>
                 </div>
                 <div ref={this.ref.sec2} style={{
                     fontSize: 30,
